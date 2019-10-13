07e6db6170b772290f3335a0fa54a43689e24df1
Karan Kadam <karankadam@gmail.com>
2019-10-14 00:47:10 +1030

    add templates, refactored routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello World!'
+    user = {'username': 'Karan'}
+    posts = [
+        {
+            'author': {'username': 'Namal'},
+            'body': 'Hmm...'
+        },
+        {
+            'author': {'username': 'Karan'},
+            'body': 'Marianne on Netflix was a scary show!'
+        },
+        {
+            'author': {'username': 'Snehal'},
+            'body': 'The Mazda MX-5 is the best looking car :)'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
